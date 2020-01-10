71cccfa9297437a5d7b7f8b6c2daa80a9cd6ee51
alex <irish3725@github.com>
2020-01-10 15:39:52 -0700

    updated README.

--- README.md
+++ README.md
@@ -5,3 +5,26 @@ pi-greenhouse is raspberry pi controlled greenhouse with a webpage front end to 
 
 The intention of this project is to combine my passion for house plants and my passion for software development. I get to gain experience with automation, web development, database management, and plant propogation at the same time.
 
+## Current Progress
+
+### Web Interface
+- [x] Complete Node.js tutorial at: https://nextjs.org/learn/basics/styling-components.
+- [] Create main page with temperature, humidity, fan, light, most recent image.
+- [] Display README on about page.
+- [] Get data from database for statistics history.
+
+### Greenhouse Automation
+- [] Temperature.
+- [] Gumidity.
+- [] Toggle fan.
+- [] Toggle light.
+- [] Get regular (daily?) image.
+
+### Database Backend
+- [] Determine length of time between readings.
+- [] Create table(s).
+- [] Determine lifespan of data and automatically remove old data.
+
+
+
+
--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,71 @@
 
-import withLayout from '../components/MyLayout';
+import { useRouter } from 'next/router';
+import Markdown from 'react-markdown';
+import Layout from '../components/MyLayout';
 
-const Page = () => <p>This is the about page</p>;
+export default () => {
 
-export default withLayout(Page);
+  const router = useRouter();
+   
+  return (
+    <Layout>
+      <h1>{router.query.id}</h1>
+      <div className="markdown">
+        <Markdown
+          source={`
+# pi-greenhouse
+
+pi-greenhouse is raspberry pi controlled greenhouse with a webpage front end to display the current conditions as well as a histo    ry of the conditions in the greenhouse.
+ 
+The intention of this project is to combine my passion for house plants and my passion for software development. I get to gain ex    perience with automation, web development, database management, and plant propogation at the same time.
+ 
+## Current Progress
+ 
+### Web Interface
+- [x]   Complete Node.js tutorial at: https://nextjs.org/learn/basics/styling-components.
+- [ ]   Create main page with temperature, humidity, fan, light, most recent image image.
+- [ ]   Display README on about page.
+- [ ]   Get data from database for statistics history.
+
+### Greenhouse Automation
+- [ ]   Temperature.
+- [ ]   Humidity.
+- [ ]   Toggle fan.
+- [ ]   Toggle light.
+- [ ]   Get regular (daily?) image.
+
+### Database Backend
+- [ ]   Determine length of time between readings.
+- [ ]   Create table(s).
+- [ ]   Determine lifespan of data and automatically remove old data.
+      `}
+        />
+      </div>
+      <style jsx global>{`
+        .markdown {
+          font-family: 'Arial';
+        }
+
+        .markdown a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        .markdown: a:hover {
+          opacity: 0.6;
+        }
+
+        .markdown h3 {
+          margin: 0;
+          padding: 0;
+          text-transform: uppercase;
+        }
+
+        .markdown ul {
+          list-style-type:none;
+        }
+      `}</style>
+    </Layout>
+  );
+}
 
