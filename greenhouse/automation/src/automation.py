d4943ce8be7f650ff5f6621c77e6b0fa9c913469
alex <irish.alexander.m@gmail.com>
2020-04-08 15:47:58 -0600

    changed and write times for sensor to 10 minutes

--- greenhouse/automation/src/automation.py
+++ greenhouse/automation/src/automation.py
@@ -9,10 +9,10 @@ import database
 import readsensor
 
 # automation constants
-WRITE_INTERVAL = 40 # time interval for writing to database (10 minutes)
-READ_INTERVAL = 10 # time interval for taking readings (10 minutes)
-OK_TEMPERATURES = [65, 80] # range of ok temperatures
-SIMULATE_READINGS = True
+WRITE_INTERVAL = 600 # time interval for writing to database (10 minutes)
+READ_INTERVAL = 600 # time interval for taking readings (10 minutes)
+OK_TEMPERATURES = [70, 87] # range of ok temperatures
+SIMULATE_READINGS = False
 
 # database constants (to be replaced by config file)
 DB_NAME = 'greenhouse_statistics'
